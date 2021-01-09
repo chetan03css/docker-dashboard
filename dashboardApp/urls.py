12423205859295d195486045cd1af3f38ea963a3
chetan03css <76621278+chetan03css@users.noreply.github.com>
2021-01-10 00:24:04 +0530

    Update urls.py

--- dashboardApp/urls.py
+++ dashboardApp/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import url
 from dashboardApp import views
 
 urlpatterns=[
-    url("home/",views.homepage),
-    url("dockerdashb/",views.dockerdashboard),
-    url("/",views.homepage),
+    url("home/",views.dockerdashboard),
+    url("/",views.dockerdashboard),
 ]
